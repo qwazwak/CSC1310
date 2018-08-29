44980eeb02a155b957086111a4a223b93450ad91
qwazwak <realqwazwak@gmail.com>
2018-08-29 17:15:55 -0500

    Start created

--- Labs/Lab-01/Text.h
+++ Labs/Lab-01/Text.h
@@ -1,7 +1,20 @@
 #ifndef TEXT_H
 	#define TEXT_H
 
-	const char * textArray;//A character string holding my “String”
-	int size; //The size of my string
+	struct Text {
+		const char * textArray;
+			//A character string holding my “String”
+		int size;
+			//The size of my string
+	};
 
+	Text * createText(const char* text);
+
+	void destroyText(Text* myText);
+
+	void displayText(Text* myText);
+
+	const char* getText(Text* myText);
+
+	int getLength(Text* myText);
 #endif /* GRANDPARENT_H */
---
+++ Labs/Lab-01/text.cpp
@@ -0,0 +1,9 @@
+Text* createText(const char* text);
+
+void destroyText(Text* myText);
+
+void displayText(Text* myText);
+
+const char* getText(Text* myText);
+
+int getLength(Text* myText);
