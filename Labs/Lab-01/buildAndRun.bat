d6a7a7f37b38cc20b981adb76c230b141387a220
qwazwak <realqwazwak@gmail.com>
2018-08-29 17:57:37 -0500

    fixed bugs

--- Labs/Lab-01/buildAndRun.bat
+++ Labs/Lab-01/buildAndRun.bat
@@ -1,5 +1,5 @@
-g++ -c Driver.cpp -std=c++98 -WALL -Wextra
-g++ -c Text.cpp -std=c++98 -WALL -Wextra
+g++ -c Driver.cpp -std=c++98 -Wall -Wextra
+g++ -c Text.cpp -std=c++98 -Wall -Wextra
 
 g++ -o Lab-01.exe Driver.o Text.o -std=c++98 -WALL -Wextra
 Lab-01.exe
---
+++ Labs/Lab-01/nppBackup/buildAndRun.bat.2018-08-29_175724.bak
@@ -0,0 +1,6 @@
+g++ -c Driver.cpp -std=c++98 -WALL -Wextra
+g++ -c Text.cpp -std=c++98 -WALL -Wextra
+
+g++ -o Lab-01.exe Driver.o Text.o -std=c++98 -WALL -Wextra
+Lab-01.exe
+pause
