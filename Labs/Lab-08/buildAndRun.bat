565005806bf3390d13a37c07c57b65de0ae873e7
Braedin Jared <braedinjared@gmail.com>
2018-10-31 17:12:30 -0500

    palindrome

--- Labs/Lab-08/buildAndRun.bat
+++ Labs/Lab-08/buildAndRun.bat
@@ -1,6 +1,3 @@
-g++ -c Driver.cpp -std=c++98 -Wall -Wextra
-g++ -c Text.cpp -std=c++98 -Wall -Wextra
-
-g++ -o Lab-01.exe Driver.o Text.o -std=c++98 -Wall -Wextra
-Lab-01.exe
+make
+Lab-08.exe
 pause
--- Labs/Lab-08/lab8given.cpp
+++ Labs/Lab-08/lab8given.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int sumOfNumbers(int);
 bool isMember(int* array, int sizeOfArray, int valueToFind);
 void stringReverser(string&, int);
-bool isPalindrome(string);
+bool isPalindrome(const string&, int, int);
 int multiply(int, int);
 
 const int ARRAY_SIZE = 10;
@@ -89,7 +89,7 @@ int main() {
 				
 				//print out whether the user's string is a palindrome or not here.
 				//when you print out the user's string, print out the uppercase version that doesn't have the spaces removed.
-				cout << "String (" << userString << ") is " << isPalindrome(userString) ? "a palindrome" : "not a palindrome\n";
+				cout << "String (" << userString << ") is " << isPalindrome(userString, 0, userString.length()-1) ? "a palindrome" : "not a palindrome\n";
 				break;
 				
 			case 5:
