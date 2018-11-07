1ade2625f785abac59b9813b22d99693c0f60b5b
Braedin Jared <braedinjared@gmail.com>
2018-11-07 17:46:01 -0600

    stuff

--- Labs/Lab-09/Lab9.cpp
+++ Labs/Lab-09/Lab9.cpp
@@ -1,18 +1,53 @@
 #include "Random.h"
+#include <fstream>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
-int main(){
 
-	int lottoNum[5] = {
-		getRandomInt(0,9),
-		getRandomInt(0,9),
-		getRandomInt(0,9),
-		getRandomInt(0,9),
-		getRandomInt(0,9)
-	};
+bool binarySearch(string s, string a[]);
 
-	
+int main()
+{
+	seedRandom();
+
+	string lotto = "";
+	string guesses[5];
+	for(int i = 0; i<5; i++){
+		lotto += to_string(getRandomInt(0,9));
+	}
+
+
+	int count = 0;
+
+	while(count < 5){
+
+		string input;
+
+		cout << "Gimme a list of 5 numbers of size 5" << endl;
+		cin >> input;
+
+		while(input.length() != 5){
+			cout << "I said of size 5" << endl;
+			cin.ignore();
+			cin >> input;
+		}
+
+		guesses[count] = input;
+
+		count++;
+	}
+
+
+	// if(binarySearch()){
+
+	// }
 
 	return 0;
+}
+
+
+bool binarySearch(string s, string a[]){
+
 }
