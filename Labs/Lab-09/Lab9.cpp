5b088a3236d29f31babd9ea54e4918b512f457c9
Braedin Jared <braedinjared@gmail.com>
2018-11-07 16:38:52 -0600

    yeet

---
+++ Labs/Lab-09/Lab9.cpp
@@ -0,0 +1,18 @@
+#include "Random.h"
+
+using namespace std;
+
+int main(){
+
+	int lottoNum[5] = {
+		getRandomInt(0,9),
+		getRandomInt(0,9),
+		getRandomInt(0,9),
+		getRandomInt(0,9),
+		getRandomInt(0,9)
+	};
+
+	
+
+	return 0;
+}
